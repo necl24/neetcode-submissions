76897d38410866e923f0081c8c18b42588ecdb91
Neil Aeron <143227398+necl24@users.noreply.github.com>
2026-05-31 18:18:33 -0400

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> result;
+        for (const auto& str: strs) {
+            vector<int> count(26,0);
+            for (char c : str) {
+                count[c-'a']++;
+            }
+            string key = "";
+            for (int i = 0; i < 26; i++) {
+                key += to_string(count[i]) + "#";
+            }
+            result[key].push_back(str);
+        }
+          vector<vector<string>> res;
+        for (const auto& pair: result) {
+          res.push_back(pair.second);
+        }
+         return res;
+    }
+};
