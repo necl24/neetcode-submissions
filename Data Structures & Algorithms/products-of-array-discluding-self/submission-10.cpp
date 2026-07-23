eb4ceaa764250f1f35a474ea111b204d0dc2d1af
Neil Aeron <143227398+necl24@users.noreply.github.com>
2026-07-22 22:17:07 -0400

    Add: products-of-array-discluding-self - submission-10

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-10.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int prefix = 1;
+        vector<int> res(nums.size(),1);
+        for (int i = 0; i < nums.size(); i++) {
+            res[i] *= prefix;
+            prefix *= nums[i];
+        }
+        int postfix = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+
+    }
+};
